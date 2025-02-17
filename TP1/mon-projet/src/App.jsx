f318a471ffc529740addb9d9c96de101921f30e9
William Brooks <william.brooks@etu.unilim.fr>
2025-02-17 21:22:46 +0100

    mise à jour TP1

--- TP1/mon-projet/src/App.jsx
+++ TP1/mon-projet/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Card from './components/Card.jsx'
 import icon from './assets/icon.webp'
+import icon2 from './assets/icon2.webp'
+import Header from './components/Header.jsx'
 
 let gary = {
   icon: icon,
@@ -14,11 +16,27 @@ let gary = {
   ]
 }
 
+let bill={
+  icon: icon2,
+  name: "Bill DO",
+  mail: "bill.do@but1.mmi",
+  description: "Lorem ipsum dolor sit amien. Lorem Ipsum dolor sit amien. Lorem Ipsum dolor sit amien.",
+  medias: [
+    {name: "Instagram", url: "https://www.instagram.com/"},
+    {name: "GitHub", url: "https://github.com"},
+    {name: "Meta", url: "https://www.meta.com/"}
+  ]
+}
+
 function App() {
   return (
-    <>
-      <Card user={gary}/>
-    </>
+    <div className="gap-10">
+      <Header />
+      <div className="flex flex-row justify-center gap-10">
+        <Card user={gary}/>
+        <Card user={bill}/>
+      </div>
+    </div>
   )
 }
 
---
+++ TP1/mon-projet/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header className="header text-left">
+            <h1 className="text-2xl font-semibold">We are born for technology</h1>
+            <p className="text-xl font-normal">We make life easier for our customers and community through reliable, affordable and useful tech innovations.</p>
+            <a href="#" className="">Learn more</a>
+        </header>
+    );
+}
